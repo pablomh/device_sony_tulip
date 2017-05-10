073f2c6734b793af8cd2f0b941d77ee67e349b41
Pablo Mendez Hernandez <pablomh@gmail.com>
2017-05-10 17:47:47 +0000

    tulip: Add mixer_paths_mtp.xml from stock

--- device.mk
+++ device.mk
@@ -24,7 +24,8 @@ PRODUCT_COPY_FILES := \
     $(DEVICE_PATH)/rootdir/system/etc/thermanager.xml:system/etc/thermanager.xml \
     $(DEVICE_PATH)/rootdir/system/etc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
     $(DEVICE_PATH)/rootdir/system/etc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
-    $(DEVICE_PATH)/rootdir/system/etc/mixer_paths.xml:system/etc/mixer_paths.xml
+    $(DEVICE_PATH)/rootdir/system/etc/mixer_paths.xml:system/etc/mixer_paths.xml \
+    $(DEVICE_PATH)/rootdir/system/etc/mixer_paths.xml:system/etc/mixer_paths_mtp.xml
 
 # Device Specific Permissions
 PRODUCT_COPY_FILES += \
